345bc807e8852c82e2861398f8320ff550364b40
kerikir <vlasov-kerik@mail.ru>
2025-12-04 13:49:52 +0300

    added the implementation Subscribe Detail Use Case in the project

--- app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
@@ -1,9 +1,18 @@
 package com.tinkoff.android_homework.domain.main.usecases
 
+import com.tinkoff.android_homework.data.network.repo.detail.SubscribeDetailRepository
 import com.tinkoff.android_homework.domain.main.entities.Detail
+import javax.inject.Inject
 
 
 interface SubscribeDetailUseCase {
 
     suspend fun invoke() : Detail
+}
+
+
+class SubscribeDetailUseCaseImpl @Inject constructor(
+    private val repository: SubscribeDetailRepository
+) : SubscribeDetailUseCase {
+    
 }
