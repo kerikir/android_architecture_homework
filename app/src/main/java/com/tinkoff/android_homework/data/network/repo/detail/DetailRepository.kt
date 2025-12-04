211ef2a5d1f7e39ac7b3c8081301a5ab127cbe35
kerikir <vlasov-kerik@mail.ru>
2025-12-04 13:21:18 +0300

    renamed the detail repository interface in the project

--- app/src/main/java/com/tinkoff/android_homework/data/network/repo/detail/DetailRepository.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/repo/detail/DetailRepository.kt
@@ -21,7 +21,7 @@ class SubscribeDetailRepositoryImpl @Inject constructor(
     private val detailApiToDbMapper: DetailApiToDbMapper,
     private val detailDbToDomainMapper: DetailDbToDomainMapper,
     private val internetChecker: InternetChecker,
-) : IDetailRepository {
+) : SubscribeDetailRepository {
     override suspend fun getDetail(id: Int): Detail {
         if (internetChecker.isInternetAvailable()) {
             val detailApi = detailService.getDetail(id)
--- app/src/main/java/com/tinkoff/android_homework/data/network/repo/detail/SubscribeDetailRepository.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/repo/detail/SubscribeDetailRepository.kt
@@ -3,7 +3,7 @@ package com.tinkoff.android_homework.data.network.repo.detail
 import com.tinkoff.android_homework.domain.main.entities.Detail
 
 
-interface IDetailRepository {
+interface SubscribeDetailRepository {
 
     suspend fun getDetail(id: Int) : Detail
 }
