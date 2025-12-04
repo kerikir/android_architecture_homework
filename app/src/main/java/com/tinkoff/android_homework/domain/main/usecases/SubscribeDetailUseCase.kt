c5bc576bb311e8fefa338f609cb64c4c27f5afcb
kerikir <vlasov-kerik@mail.ru>
2025-12-04 13:44:30 +0300

    added the declaration method in the Detail Use Case interface

--- app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
@@ -1,6 +1,9 @@
 package com.tinkoff.android_homework.domain.main.usecases
 
+import com.tinkoff.android_homework.domain.main.entities.Detail
+
+
 interface SubscribeDetailUseCase {
 
-    
+    suspend fun invoke() : Detail
 }
